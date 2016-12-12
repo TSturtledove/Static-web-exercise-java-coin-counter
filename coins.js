a15585e85b61846b7b6ea660e6a086586b1e2592
TSturtledove <roberttortorelli@yahoo.com>
2016-12-12 13:21:59 -0600

    changed userAmount back to id, trying to get console.logs to fire

--- coins.js
+++ coins.js
@@ -1,12 +1,17 @@
 
 function coinamount() {
 
-var money = parseInt(document.getElementsByClassName("userAmount").value)
+  var money = document.getElementById("userAmount").value
 
+  money = parseInt(money)
 
-
-console.log(money)
+  console.log(money)
+  counter()
+  print()
 }
+
+
+
 document.addEventListener("keyup", function(e)
 {
   if(e.code=="Enter"){
@@ -19,18 +24,31 @@ var quarters
 var dimes
 var nickels
 
-for (t = someamount; t!==0;) {
-  if (t>25){
-    t= t-25
-    quarters+=1
-  }else if(t<25 && t>10){
-    t= t-10
-    dimes+=1
-  }else if(t<10 && t>5){
-    t= t-5
-    nickels+=1
-  }else if(t<5){
-    t= t-1
-    pennies+=1
+
+function counter() {
+  if (money!==NaN){
+    for (t = money; t!==0;) {
+      if (t>25){
+        t= t-25
+        quarters+=1
+      }else if(t<25 && t>10){
+        t= t-10
+        dimes+=1
+      }else if(t<10 && t>5){
+        t= t-5
+        nickels+=1
+      }else if(t<5){
+        t= t-1
+        pennies+=1
+      }
+    }
   }
+}
+
+
+function print() {
+  console.log("quarters:"quarters)
+  console.log("dimes:"dimes)
+  console.log("nickels:"nickels)
+  console.log("pennies:"pennies)
 }
